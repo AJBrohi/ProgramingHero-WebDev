79b74680f59d4289ed35d7f0348fea946649211a
Al-Junaed Islam <brohi96@gmail.com>
2021-02-06 16:54:07 +0600

    assigment 4 done, yay :)

---
+++ Assignments/4-airplane-ticket/js/script.js
@@ -0,0 +1,58 @@
+function ticketUnitChange(ticketClass, isIncrease){
+    const ticketCount = getElementOf(ticketClass+'-class-input');
+    const ticketNumber = parseInt(ticketCount.value);
+    const ticketNewNumber = countingTicket(ticketNumber, isIncrease);
+    ticketCount.value = ticketNewNumber;
+
+    const price = getElementOf(ticketClass+'-class-price');
+    price.innerText = changePrice(ticketClass, ticketNewNumber);
+
+    totalPriceCalculation();
+}
+
+function countingTicket(ticketNumber, increase) {
+    let ticketInNumber = 0;
+    if (increase == true) {
+        ticketInNumber = ticketNumber+1;
+    }
+    if (increase == false && ticketInNumber > 0){
+        ticketInNumber = ticketNumber-1;
+    }
+    return ticketInNumber;
+}
+
+function changePrice(ticketClass, ticketNewNumber) {
+    let ticketPrice = 0;
+    if (ticketClass == 'first') {
+        ticketPrice = ticketNewNumber * 150;
+    } else {
+        ticketPrice = ticketNewNumber * 100;
+    }
+    return ticketPrice;
+}
+
+function totalPriceCalculation() {
+    const firstClassTicketPriceNumber = getInt('first-class-price');    
+    const economyClassTicketPriceNumber = getInt('economy-class-price');
+    
+    const total = firstClassTicketPriceNumber + economyClassTicketPriceNumber;
+    getElementOf('subtotal').innerText = total;
+
+    const totalVat = getInt('subtotal') * 0.1;
+    getElementOf('vat').innerText = totalVat;
+
+    getElementOf('grand-total').innerText = total + totalVat;
+}
+
+function getElementOf(id, needText) {
+    return document.getElementById(id);
+}
+
+function getInt(id) {
+    return parseInt(document.getElementById(id).innerText);
+}
+
+function buy() {
+    confirm('Are you sure?');
+    alert('Thank you for purchasing from us.\n Get ready to fly.');
+}
